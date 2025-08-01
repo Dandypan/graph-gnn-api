8e297cf8c851b934f9542e308b22ae58162834f8
Dandypan <pch110737@gmail.com>
2025-08-01 10:41:10 +0800

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,16 +1,16 @@
-
 from pydantic import BaseModel
+from typing import Literal
 
-class GNNConfig(BaseModel):
-    algorithm: str
+class GNNRequest(BaseModel):
+    algorithm: Literal['GCN', 'GIN']
     learning_rate: float
-    activation: str
-    regularization: str
+    activation: Literal['ReLU', 'Sigmoid', 'Tanh']
+    regularization: Literal['L1', 'L2', 'None']
     regularization_rate: float
-    problem_type: str
+    problem_type: Literal['Classification', 'Regression']
     epoch: int
     hidden_layers: int
-    train_test_ratio: float
+    train_test_ratio: float  # 0.0 ~ 1.0
     noise_1: float
     noise_2: float
     batch_size: int
