bd78f68c14d0b20dfea839b1c0cdc600ee62cf60
Dandypan <pch110737@gmail.com>
2025-08-05 10:01:33 +0800

    Update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,51 +1,50 @@
 import networkx as nx
 from torch_geometric.data import Data
 import torch
+import random
 
 
-def  load_default_graph() -> Data:
+def load_default_graph(noise_1=0.0, noise_2=0.0) -> Data:
     G = nx.Graph()
 
-    # Clique 1: 10 nodes
+    # Define cliques
     clique1 = [f"n{i}" for i in range(10)]
-    G.add_nodes_from(clique1)
-    G.add_edges_from([(u, v) for i, u in enumerate(clique1) for v in clique1[i + 1:]])
-
-    # Clique 2: 15 nodes
     clique2 = [f"n{i}" for i in range(10, 25)]
-    G.add_nodes_from(clique2)
-    G.add_edges_from([(u, v) for i, u in enumerate(clique2) for v in clique2[i + 1:]])
-
-    # Clique 3: 12 nodes
     clique3 = [f"n{i}" for i in range(25, 37)]
-    G.add_nodes_from(clique3)
-    G.add_edges_from([(u, v) for i, u in enumerate(clique3) for v in clique3[i + 1:]])
-
-    # Clique 4: 8 nodes
     clique4 = [f"n{i}" for i in range(37, 45)]
-    G.add_nodes_from(clique4)
-    G.add_edges_from([(u, v) for i, u in enumerate(clique4) for v in clique4[i + 1:]])
-
-    # Clique 5: 20 nodes
     clique5 = [f"n{i}" for i in range(45, 65)]
-    G.add_nodes_from(clique5)
-    G.add_edges_from([(u, v) for i, u in enumerate(clique5) for v in clique5[i + 1:]])
 
-    # Connect cliques with bridges
-    bridges = [(clique1[0], clique2[0]), (clique2[-1], clique3[0]), (clique3[-1], clique4[0]), (clique4[-1], clique5[0])]
+    cliques = [clique1, clique2, clique3, clique4, clique5]
+    for clique in cliques:
+        G.add_nodes_from(clique)
+        G.add_edges_from([(u, v) for i, u in enumerate(clique) for v in clique[i + 1:]])
+
+    # Add bridges between cliques
+    bridges = [(clique1[0], clique2[0]), (clique2[-1], clique3[0]),
+               (clique3[-1], clique4[0]), (clique4[-1], clique5[0])]
     G.add_edges_from(bridges)
 
-    # Add some noise edges
-    import random
-    all_nodes = list(G.nodes())
-    for _ in range(50):
-        u, v = random.sample(all_nodes, 2)
-        if u != v and not G.has_edge(u, v):
-            G.add_edge(u, v)
-
+    # Apply node relabeling for tensor indexing
     mapping = {node: i for i, node in enumerate(G.nodes())}
     G = nx.relabel_nodes(G, mapping)
 
+    # Add noise_1 type: random noise edges
+    if noise_1 > 0.0:
+        total_possible = G.number_of_nodes() * (G.number_of_nodes() - 1) // 2
+        existing_edges = G.number_of_edges()
+        max_noise_edges = total_possible - existing_edges
+        noise_edges_to_add = int(noise_1 * G.number_of_edges())
+
+        added = 0
+        while added < noise_edges_to_add and added < max_noise_edges:
+            u, v = random.sample(range(G.number_of_nodes()), 2)
+            if not G.has_edge(u, v):
+                G.add_edge(u, v)
+                added += 1
+
+    # TODO: Add noise_2 logic if needed (e.g. node feature noise or label flipping)
+
+    # Build PyG Data object
     edge_index = torch.tensor(list(G.edges()), dtype=torch.long).t().contiguous()
     x = torch.ones((G.number_of_nodes(), 10), dtype=torch.float)
     y = torch.randint(0, 2, (G.number_of_nodes(),), dtype=torch.long)
